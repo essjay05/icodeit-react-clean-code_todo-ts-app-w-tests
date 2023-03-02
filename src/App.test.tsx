2bc0388816535d9969c1c8f8cadfa0a34350a13b
Joy Serquiña <stepjoyous@gmail.com>
2023-03-02 15:13:21 -0800

    First commit. Learning to write tests.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test(`renders 'my todo application' text`, () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('My Todo Application');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders submit button', () => {
+  render(<App />);
+  const submitBtnElement = screen.getByText('Submit');
+  expect(submitBtnElement).toBeInTheDocument();
 });
